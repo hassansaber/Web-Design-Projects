503bbe409845bc0daf99c3582f1553092c7607fd
hassansaber <hassansaberkarin@gmail.com>
2023-10-23 13:58:45 +0330

    almost done

--- 09-Omnifood-Optimizations/script.js
+++ 09-Omnifood-Optimizations/script.js
@@ -1,3 +1,64 @@
+// set current year
+const yearEl = document.querySelector(".year")
+const currentYear = new Date().getFullYear()
+yearEl.textContent = currentYear
+
+// Male mobile nav work
+const BtnMobileNavEl = document.querySelector(".btn-mobile-nav")
+const headerEl = document.querySelector(".header")
+BtnMobileNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open")
+})
+
+// Smooth Scrolling Animation
+const allLinks = document.querySelectorAll("a:link")
+allLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href")
+
+    // scroll back to top
+    if (href === "#") window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile nav 
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open")
+  })
+})
+
+// Sticky navigation.
+const sectionHeroEl = document.querySelector(".section-hero")
+const obs = new IntersectionObserver((entries) => {
+  const ent = entries[0]
+  console.log(ent);
+  if (ent.isIntersecting === false)
+    document.body
+      .classList.add('sticky')
+
+  if (ent.isIntersecting === true)
+    document.body
+      .classList.remove('sticky')
+
+
+      , {
+      // In the viewport
+      root: null,
+      threshold: 0,
+      rootMargin: "-80px" // height 8rem for nav
+    }
+})
+obs.observe(sectionHeroEl)
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
